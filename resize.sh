2fdf7c61c18fc8545be0fba49f461b56dc973cbf
Vincent Ramdhanie <vramdhanie@gmail.com>
2019-05-27 08:38:03 -0400

    Add resize script to automatically format and reduce png images to jpg
    Depends on imagmagick mogrify. This script simply accepts a filename and attempts perform the conversation

---
+++ resize.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+size=500x
+quality=80
+
+if [[ -z $1 ]]; then
+  echo Please specify a file name
+  exit 1
+fi
+
+echo About to mogrify $1
+
+filename="${1%.*}"
+
+# echo File name without extension $filename
+
+# mv $1 temp.png
+
+mogrify -format jpg $1
+mogrify -resize $size $filename.jpg
+mogrify -strip -quality $quality $filename.jpg
+
+rm $1
